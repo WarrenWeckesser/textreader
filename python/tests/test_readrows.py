9c7aef9fe59767d5befc40ab13789e76677282bc
Warren Weckesser <warren.weckesser@enthought.com>
2012-03-06 22:16:43 -0600

    TST: Added another python test.

--- python/tests/test_readrows.py
+++ python/tests/test_readrows.py
@@ -2,15 +2,26 @@
 from datetime import datetime
 import os
 import numpy as np
-from numpy.testing import assert_array_equal, assert_equal, assert_
+from numpy.testing import assert_array_equal, assert_equal
 from textreader import readrows
 
 
 filename = 'tmp.txt'
 
 
+def my_assert_array_equal(a, b):
+    if np.__version__ == '1.4.1':
+        # XXX Test fixed for 1.4.1. I haven't tracked down why assert_array_equal
+        # fails in 1.4.1.  I also haven't tried it with 1.5.1.
+        assert_equal(a.shape, b.shape)
+        for arow, brow in zip(a, b):
+            assert_array_equal(arow, brow)
+    else:
+        assert_array_equal(a, b)
+
+
 def test1():
-    dt = np.dtype([('x', float),('y', float)])
+    dt = np.dtype([('x', float), ('y', float)])
     a = np.array([(1.0, 2.0), (3.0, 4.5)], dtype=dt)
     for sep in ' ,|':
         for fmt in ['%.18e', '%.2f', '%g']:
@@ -24,9 +35,9 @@ def test2():
     nrows = 100
     ncols = 50
     dt = np.dtype([('x' + str(k), np.float64) for k in range(ncols)])
-    a = np.arange(float(nrows*ncols)).reshape(nrows,ncols).view(dt).squeeze()
+    a = np.arange(float(nrows * ncols)).reshape(nrows, ncols).view(dt).squeeze()
     for sep in ' ,|':
-        for fmt in ['%.18e','%.2f']:
+        for fmt in ['%.18e', '%.2f']:
             np.savetxt(filename, a, delimiter=sep, fmt=fmt)
             b = readrows(filename, dt, delimiter=sep)
             assert_array_equal(a, b)
@@ -51,8 +62,9 @@ def test3():
         (datetime(2011, 1, 2, 0, 30, 0, 0), 1.0, 15, 'FR'),
         (datetime(2011, 1, 2, 0, 45, 0, 0), 1.25, 16, 'TG'),
         (datetime(2011, 1, 2, 0, 56, 0, 0), 1.5, 17, 'NK'),
-        (datetime(2011, 1, 2, 1, 13, 0, 0), 1.0, 18, 'QQ')], dtype= dt)
-    b = readrows(filename, dt, delimiter=',', usecols=(0,1,-2,-1), datetime_fmt="%Y-%m-%d %H:%M", tzoffset=None)
+        (datetime(2011, 1, 2, 1, 13, 0, 0), 1.0, 18, 'QQ')], dtype=dt)
+    b = readrows(filename, dt, delimiter=',', usecols=(0, 1, -2, -1),
+                 datetime_fmt="%Y-%m-%d %H:%M", tzoffset=None)
     assert_array_equal(a, b)
     os.remove(filename)
 
@@ -70,20 +82,15 @@ def test4():
 
     dt = np.dtype([('x', np.float32), ('codes', np.uint8, 4)])
     a = np.array([
-        (1.0,(1,2,3,4)),
-        (10.0,(11,12,13,14)),
-        (100.0,(21,22,23,24))], dtype=dt)
+        (1.0, (1, 2, 3, 4)),
+        (10.0, (11, 12, 13, 14)),
+        (100.0, (21, 22, 23, 24))], dtype=dt)
     b = readrows(filename, dt, delimiter=',')
 
-    if np.__version__ == '1.4.1':
-        # XXX Test fixed for 1.4.1. I haven't tracked down why assert_array_equal
-        # fails in 1.4.1.  I also haven't tried it with 1.5.1.
-        assert_equal(a.shape, b.shape)
-        for arow, brow in zip(a,b):
-            assert_array_equal(arow, brow)
-    else:
-        assert_array_equal(a, b)
+    my_assert_array_equal(a, b)
+
     os.remove(filename)
+
 
 def test5():
     text = """\
@@ -98,13 +105,34 @@ def test5():
     dt = np.dtype([('x', np.complex128), ('y', np.complex128)])
     a = np.array([(1.0-2.0j, 3.0+4.0j), (0.5, 6.0)], dtype=dt)
     b = readrows(filename, dt, delimiter=',')
+    my_assert_array_equal(a, b)
 
-    if np.__version__ == '1.4.1':
-        # XXX Test fixed for 1.4.1. I haven't tracked down why assert_array_equal
-        # fails in 1.4.1.  I also haven't tried it with 1.5.1.
-        assert_equal(a.shape, b.shape)
-        for arow, brow in zip(a,b):
-            assert_array_equal(arow, brow)
-    else:
-        assert_array_equal(a, b)
+    os.remove(filename)
+
+
+def test6():
+    text = """\
+  1.0,  1,  2,  3,  4
+ 10.0, 11, 12, 13, 14
+100.0, 21, 22, 23, 24
+"""
+
+    f = open(filename, 'w')
+    f.write(text)
+    f.close()
+
+    f = open(filename, 'r')
+
+    dt = np.dtype([('x', np.float32), ('codes', np.uint8, 4)])
+    a = np.array([(1.0, (1, 2, 3, 4))], dtype=dt)
+    b = readrows(f, dt, delimiter=',', numrows=1)
+    my_assert_array_equal(a, b)
+
+    a = np.array([
+        (10.0, (11, 12, 13, 14)),
+        (100.0, (21, 22, 23, 24))], dtype=dt)
+    b = readrows(f, dt, delimiter=',', numrows=2)
+    my_assert_array_equal(a, b)
+
+    f.close()
     os.remove(filename)
