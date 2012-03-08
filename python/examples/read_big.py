a0e9f71254a99842877cf9b77bf41f2df42f8d39
Warren Weckesser <warren.weckesser@enthought.com>
2012-03-08 13:56:34 -0600

    read_big.py now reads a 2D array instead of a structured array.

--- python/examples/read_big.py
+++ python/examples/read_big.py
@@ -10,10 +10,18 @@ filename = 'data/big.csv'
 if not os.path.exists(filename):
     print "{name} does not exist. Run the script generate_big.py to generate {name}.".format(name=filename)
 else:
-    fields = [('f'+str(k), np.float32) for k in range(50)]
-    dt = np.dtype(fields)
+    dt = np.float32
 
     t0 = time.time()
     a = readrows(filename, dtype=dt, delimiter=',')
     t1 = time.time()
     print t1 - t0, "seconds"
+
+    f = open(filename, 'r')
+    first_two = f.readline().split(',')[:2]
+    f.seek(-64, 2)
+    last_two = f.readline().strip().split(',')[-2:]
+    f.close()
+    print "First two samples in the file:", first_two,  "  Data in array:", a[0,:2]
+    print "Last two samples in the file:", last_two,  "  Data in array:", a[-1,-2:]
+    print "Data has been converted to", dt
